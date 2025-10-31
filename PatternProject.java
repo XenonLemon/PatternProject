c718b0c19fddba3ecddd07b145e6fe837e7da542
XenonLemon <pd922503@gmail.com>
2025-10-31 18:29:36 +0000

    Commit 2 10/31

--- PatternProject.java
+++ PatternProject.java
@@ -1,6 +1,8 @@
 //Peter Daniel
 // 10/30/25
-//
+//Pattern Project is meant to output a pattern based on an integer input.
 public class PatternProject{
-    
+    public static void stars(int rows){
+
+    }
 }
