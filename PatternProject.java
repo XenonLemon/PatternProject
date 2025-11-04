1e31bf339e91584b2788e07d019a2c3c0e138fd2
XenonLemon <pd922503@gmail.com>
2025-11-04 19:29:59 +0000

    Commit 3 11/4

--- PatternProject.java
+++ PatternProject.java
@@ -2,7 +2,31 @@
 // 10/30/25
 //Pattern Project is meant to output a pattern based on an integer input.
 public class PatternProject{
+    //stars will require an int in order to determine how many rows of stars will be printed out.
+    //Using rows, stars will create a rows amount of stars, increasing by 2 stars each row.
     public static void stars(int rows){
+        int i = 0;
+        String stars= "*";
+        while(i < rows){
+            i++;
+            System.out.println(stars);
+            stars += "**";
+        }
+    }
+    //
+    public static void triangle(int rows){
+        int i = 0;
+        while(i < rows){
+            i++;
+            System.out.print(i);
+            while(){
+              System.out.println(i);  
+            }
+        }
 
+    }
+    public static void main(String[] args){
+        stars(7);
+        triangle(9);
     }
 }
